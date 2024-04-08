a6c96c52a1454763881f288a2ae8e84d577a8e92
Nirbhay <nirbhaysingh943012@gmail.com>
2024-04-08 08:20:09 +0530

    connect-to-mongo-atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const PORT = process.env.PORT || 7000;
-const MONGO_URL = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${MONGO_URL}/${DB_NAME}`
-    );
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URL);
     console.log(
       `\n Mongo DB connected !! DB HOST : ${connectionInstance.connection.host}`
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 
 const app = express();
-app.use(bodyParser.json());
-dotenv.config({ path: "./env" });
+const PORT = process.env.PORT || 7000;
+
+dotenv.config();
+
 connectDB();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>This is homepage</h1>`);
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 /*
 
 ******     First Approach  ******
