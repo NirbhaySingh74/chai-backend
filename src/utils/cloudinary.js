6cd8657f3df224cd84ca0a24846102f89ddef130
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-04-10 10:13:08 +0530

    bug fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,13 +7,19 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log(process.env.CLOUDINARY_API_KEY); // i am geeting undefined here
+
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    console.log(localFilePath);
+
+    // console.log(localFilePath);
+
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
+
+    console.log("rsponse", response);
 
     // file has been uploaded successfull
     console.log("file is uploaded on cloudinary ", response.url);
@@ -21,7 +27,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     // console.log(response.format);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    // fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
